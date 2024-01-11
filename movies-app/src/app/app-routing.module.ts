654edf2e6b062478d1219a724db0cd3409527225
Habiba Gamil <habiba.gamil2000@gmail.com>
2024-01-11 00:08:38 -0800

    router fixed +unit testing

--- movies-app/src/app/app-routing.module.ts
+++ movies-app/src/app/app-routing.module.ts
@@ -2,16 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './authentication/components/login/login.component';
 import { AuthGaurdService } from './authentication/services/auth-gaurd.service';
-import { MovieItemComponent } from './core/components/movie-item/movie-item.component';
-import { MovieComponent } from './core/components/movie/movie.component';
+import { MovieComponent } from './core/components/movie-page/movie.component';
 import { MoviesComponent } from './core/components/movies/movies.component';
 
 const routes: Routes = [
- 
-  {path: "explore", component: MoviesComponent, canActivate: [AuthGaurdService]},
-  {path: "movie/:id", component: MovieComponent,canActivate: [AuthGaurdService] },
-  {path: "login", component: LoginComponent},
-  {path: '', redirectTo: "explore", pathMatch:'full'}
+  {path: '', redirectTo: "explore", pathMatch:'full'},
+
+  {path: "login", component: LoginComponent,
+  loadChildren: ()=> 
+  import('./core/core.module').then(
+     (mod) => mod.CoreModule
+  )},
+
+  {path: "explore", component: MoviesComponent, 
+    canActivate: [AuthGaurdService],
+    loadChildren: ()=> 
+    import('./authentication/authentication.module').then(
+      (mod) => mod.AuthenticationModule
+    )},
+
+  {path: "movie/:id", component: MovieComponent,canActivate: [AuthGaurdService] }
+  
 ];
 
 @NgModule({
